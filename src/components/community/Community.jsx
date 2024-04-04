00a5b1074afef34b18062f45b3175538981da6e5
Lior Fridman <76394602+liorspidy@users.noreply.github.com>
2024-04-04 19:44:51 +0300

    .

--- src/components/community/Community.jsx
+++ src/components/community/Community.jsx
@@ -1,10 +1,18 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import classes from "./Community.module.css";
 import { motion } from "framer-motion";
+import axios from "axios";
 
 const Community = ({ isMobile }) => {
   const [isSubmitted, setIsSubmitted] = useState(false);
-  const titleClass = isMobile ? classes.titleContainerMobile : classes.titleContainer;
+  const [subscriberName, setSubscriberName] = useState("");
+  const [subscriberEmail, setSubscriberEmail] = useState("");
+  const [subscriberPhone, setSubscriberPhone] = useState("");
+  const [subscriberError, setSubscriberError] = useState("");
+
+  const titleClass = isMobile
+    ? classes.titleContainerMobile
+    : classes.titleContainer;
   const communityClass = isMobile ? classes.communityMobile : classes.community;
   const formClass = isMobile ? classes.formMobile : classes.form;
   const inputContainerClass = isMobile
@@ -13,19 +21,89 @@ const Community = ({ isMobile }) => {
   const formButtonClass = isMobile
     ? classes.formButtonMobile
     : classes.formButton;
-  const aboutBoxVarients = {
+  const aboutBoxVariants = {
     offscreen: { x: 30, opacity: 0 },
     onscreen: { x: 0, opacity: 1, transition: { duration: 2 } },
   };
 
-  const sumbitHandler = (e) => {
+  const sumbitHandler = async (e) => {
     e.preventDefault();
-    setIsSubmitted(true);
+  
+    // Validation and formatting checks
+    let name = subscriberName.trim(); // Trim leading and trailing spaces
+    if (name.length < 2) {
+      setSubscriberError("שם מלא חייב להכיל לפחות שני תווים");
+      return;
+    }
+  
+    let phone = subscriberPhone.replace(/\D/g, ''); 
+    if (!/^\d{9,10}$/.test(phone)) {
+      setSubscriberError("מספר טלפון לא תקין");
+      return;
+    }
+    
+    phone = phone.replace(/^0?(\d{2})(\d{3})(\d{4})$/, '0$1-$2$3');
+    
+    if (!/^\d{3}-\d{7}$/.test(phone)) {
+      setSubscriberError("מספר טלפון לא תקין");
+      return;
+    }
+    
+    try {
+      const mylistId = process.env.REACT_APP_MAIN_LIST_ID;
+      const subscriber = {
+        name: name,
+        email: subscriberEmail,
+        phone: phone,
+      };
+  
+      await axios.post(
+        `http://localhost:5000/ravmesser/addSubscriber/${mylistId}`,
+        subscriber
+      );
+      setIsSubmitted(true);
+      setSubscriberError("");
+      setSubscriberName("");
+      setSubscriberEmail("");
+      setSubscriberPhone("");
+    } catch (err) {
+      console.log(err);
+      setSubscriberError(
+        "קרתה שגיאה בשליחת הטופס, אנא נסו בשנית בעוד כמה רגעים"
+      );
+    }
   };
 
+  useEffect( () => {
+
+    const getLists = async () => {
+      try {
+        const res = await axios.get("http://localhost:5000/ravmesser/getLists");
+        console.log(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+
+    const getSubscribers = async () => {
+      try {
+        const mylistId = process.env.REACT_APP_MAIN_LIST_ID;
+        const res = await axios.get(
+          `http://localhost:5000/ravmesser/getSubscribers/${mylistId}`
+        );
+        console.log(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+
+    // getLists();
+    // getSubscribers();
+  },[])
+  
   return (
     <div className={communityClass}>
-      <motion.div className={titleClass} variants={aboutBoxVarients}>
+      <motion.div className={titleClass} variants={aboutBoxVariants}>
         <h3 className={classes.title}>הצטרפו לקהילה</h3>
       </motion.div>
       <form className={formClass} onSubmit={sumbitHandler}>
@@ -33,22 +111,40 @@ const Community = ({ isMobile }) => {
           <div className={classes.unsubmitted}>
             <div className={inputContainerClass}>
               <label htmlFor="name">
-                שם
-                <input type="text" />
+                שם מלא
+                <input
+                  id="name"
+                  type="text"
+                  value={subscriberName}
+                  onChange={(e) => setSubscriberName(e.target.value)}
+                />
               </label>
             </div>
             <div className={inputContainerClass}>
               <label htmlFor="email">
                 אימייל
-                <input type="email" />
+                <input
+                  id="email"
+                  type="email"
+                  value={subscriberEmail}
+                  onChange={(e) => setSubscriberEmail(e.target.value)}
+                />
               </label>
             </div>
             <div className={inputContainerClass}>
               <label htmlFor="phone">
                 טלפון
-                <input type="tel" />
+                <input
+                  id="phone"
+                  type="tel"
+                  value={subscriberPhone}
+                  onChange={(e) => setSubscriberPhone(e.target.value)}
+                />
               </label>
             </div>
+            {subscriberError.length > 0 && (
+              <p className={classes.errorMessage}>{subscriberError}</p>
+            )}
             <div className={formButtonClass}>
               <button type="submit">הצטרף</button>
             </div>
